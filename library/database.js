2b29c8ccc31db86da69a8a20f9bbbc17da82286d
iqbal rahman <iqbal070103@gmail.com>
2023-12-23 23:43:26 +0700

    Version 1.1

--- library/database.js
+++ library/database.js
@@ -1,18 +1,18 @@
 let mysql = require('mysql');
  
-// let connection = mysql.createConnection({
-//    host:        'db4free.net',
-//    user:        'transprima',
-//    password:    'Iqbal2507',
-//    database:    'transprima'
-//  });
-
 let connection = mysql.createConnection({
-  host:        'localhost',
-  user:        'root',
-  password:    '',
-  database:    'dompetku'
-});
+   host:        'db4free.net',
+   user:        'transprima',
+   password:    'Iqbal2507',
+   database:    'transprima'
+ });
+
+// let connection = mysql.createConnection({
+//   host:        'localhost',
+//   user:        'root',
+//   password:    '',
+//   database:    'dompetku'
+// });
 
 connection.connect(function(error){
    if(!!error){
